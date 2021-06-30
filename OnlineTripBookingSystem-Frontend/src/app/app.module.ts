21f87682fa8d26d49e8ee892b5e42d7cb091759b
subani7 <84901768+subani7@users.noreply.github.com>
2021-06-30 14:13:51 +0530

    components

--- OnlineTripBookingSystem-Frontend/src/app/app.module.ts
+++ OnlineTripBookingSystem-Frontend/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
