ce90728c92814a63020e4bee081b1774918fd7c6
shouryadasari413 <shouryadasari413@gmail.com>
2021-07-01 19:43:34 +0530

    login

--- OnlineTripBookingSystem-Frontend/src/app/login/login.component.ts
+++ OnlineTripBookingSystem-Frontend/src/app/login/login.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginService } from '../shared/login.service';
 
 export class JwtResponse{
 
   jwtToken!:string;
+  id!:number;
 }
 
 @Component({
@@ -13,25 +13,23 @@ export class JwtResponse{
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-
 export class LoginComponent implements OnInit {
 
   email:string = '';
   password:string = '';
   userType:string = '';
   invalidLogin:boolean = false;
-  response!:JwtResponse;
-  loginForm!: FormGroup;
+  response!:JwtResponse
 
   constructor(private router: Router,
-    private loginService: LoginService) { }
+    private loginservice: LoginService) { }
 
   ngOnInit() {
     
   }
 
   checkLogin() {
-    (this.loginService.authenticate(this.email, this.password, this.userType).subscribe(
+    (this.loginservice.authenticate(this.email, this.password, this.userType).subscribe(
       data => {
         console.log(data)
         this.router.navigate([''])
@@ -42,12 +40,78 @@ export class LoginComponent implements OnInit {
       }
     )
     );
+
   }
 
   setValues(data:any){
     console.log("in set values",data)
     sessionStorage.setItem('email',this.email);
-        sessionStorage.setItem('token',data.token);
+        sessionStorage.setItem('token',data.token)
+       
       console.log('data set')
+
+
+
   }
+
 }
+
+
+
+
+
+
+
+// import { Component, OnInit } from '@angular/core';
+// import { FormGroup } from '@angular/forms';
+// import { Router } from '@angular/router';
+// import { LoginService } from '../shared/login.service';
+
+// export class JwtResponse{
+
+//   jwtToken!:string;
+// }
+
+// @Component({
+//   selector: 'app-login',
+//   templateUrl: './login.component.html',
+//   styleUrls: ['./login.component.css']
+// })
+
+// export class LoginComponent implements OnInit {
+
+//   email:string = '';
+//   password:string = '';
+//   userType:string = '';
+//   invalidLogin:boolean = false;
+//   response!:JwtResponse;
+//   loginForm!: FormGroup;
+
+//   constructor(private router: Router,
+//     private loginService: LoginService) { }
+
+//   ngOnInit() {
+    
+//   }
+
+//   checkLogin() {
+//     (this.loginService.authenticate(this.email, this.password, this.userType).subscribe(
+//       data => {
+//         console.log(data)
+//         this.router.navigate([''])
+//         this.invalidLogin = false
+//       },
+//       error => {
+//         this.invalidLogin = true
+//       }
+//     )
+//     );
+//   }
+
+//   setValues(data:any){
+//     console.log("in set values",data)
+//     sessionStorage.setItem('email',this.email);
+//         sessionStorage.setItem('token',data.token);
+//       console.log('data set')
+//   }
+// }
