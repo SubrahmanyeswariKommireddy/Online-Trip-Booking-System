635404da52a8f11bbaa7d83490d2dd90ee898d92
SubrahmanyeswariKommireddy <59472440+SubrahmanyeswariKommireddy@users.noreply.github.com>
2021-07-01 10:55:07 +0530

    components

---
+++ OnlineTripBookingSystem-Frontend/src/app/home/default-homepage/default-homepage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-default-homepage',
+  templateUrl: './default-homepage.component.html',
+  styleUrls: ['./default-homepage.component.css']
+})
+export class DefaultHomepageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- OnlineTripBookingSystem-Frontend/src/app/login/login.component.ts
+++ OnlineTripBookingSystem-Frontend/src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-// import { LoginService } from '../shared/login.service';
+import { LoginService } from '../shared/login.service';
 
 // export class JwtResponse{
 //   jwtToken:string='';
@@ -12,14 +12,33 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  // email:string = '';
-  // password:string = '';
-  // userType:string ='';
-  // invalidLogin:boolean = false;
-  // response!:JwtResponse
+  email:string = '';
+  password:string = '';
+  userType:string ='';
+  invalidLogin:boolean = false;
+  //response!:JwtResponse
 
-  // constructor(private loginService : LoginService, private router:Router) { }
+  constructor(private loginService : LoginService, private router:Router) { }
 
   ngOnInit(): void {
   }
+
+  // checkLogin() {
+  //   (this.loginService.authenticate(this.email, this.password,this.userType).subscribe(
+  //     data => {
+  //       console.log(data)
+  //       /* this.response=data.jwtToken;
+  //       console.log("Rseponse" ,this.response,"response ") */
+  //       //this.setValues(data);
+  //       this.router.navigate([''])
+  //       this.invalidLogin = false
+  //     },
+  //     error => {
+  //       this.invalidLogin = true
+
+  //     }
+  //   )
+  //   );
+
+  // }
 }
