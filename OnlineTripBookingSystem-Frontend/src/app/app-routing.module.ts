df0a33f5813ed11ec78a376d270476b9d9d3ef83
shouryadasari413 <shouryadasari413@gmail.com>
2021-07-02 12:59:46 +0530

    bus view

--- OnlineTripBookingSystem-Frontend/src/app/app-routing.module.ts
+++ OnlineTripBookingSystem-Frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BusViewComponent } from './bus-view/bus-view.component';
 import { SearchBusesComponent } from './bus/search-buses/search-buses.component';
 import { DefaultHomepageComponent } from './home/default-homepage/default-homepage.component';
 import { LoginComponent } from './login/login.component';
@@ -9,7 +10,8 @@ const routes: Routes = [
   {path:'',component:DefaultHomepageComponent},
   {path:'login',component:LoginComponent},
   {path:'signup',component:SignupComponent},
-  {path:'search',component:SearchBusesComponent}
+  {path:'search',component:SearchBusesComponent},
+  {path:'viewSeats',component:BusViewComponent}
 ];
 
 @NgModule({
