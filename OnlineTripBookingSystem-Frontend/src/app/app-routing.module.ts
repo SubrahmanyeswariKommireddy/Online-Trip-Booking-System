476ea8bc4fed53a53150f7fb731ef8a0722e7043
Susmitha0498 <susmideekshi@gmail.com>
2021-07-02 20:46:10 +0530

    Admin home page

--- OnlineTripBookingSystem-Frontend/src/app/app-routing.module.ts
+++ OnlineTripBookingSystem-Frontend/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminHomepageComponent } from './home/admin-homepage/admin-homepage.component';
 import { DefaultHomepageComponent } from './home/default-homepage/default-homepage.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   {path:'',component:DefaultHomepageComponent},
+  {path:'home',component:AdminHomepageComponent},
   {path:'login',component:LoginComponent},
   {path:'signup',component:SignupComponent}
 ];
