621892f6565549f2e3bc7b4f269ba65322cf97cc
shouryadasari413 <shouryadasari413@gmail.com>
2021-07-05 15:17:39 +0530

    changes

--- OnlineTripBookingSystem-Frontend/src/app/shared/user.service.ts
+++ OnlineTripBookingSystem-Frontend/src/app/shared/user.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { Customer } from "../models/Customer";
 import { User } from "../models/User";
 
 @Injectable({
@@ -15,10 +16,25 @@ import { User } from "../models/User";
 
     constructor(private httpClient: HttpClient) { }
 
-    updateUser(id:number,user:User):Observable<User>{
+    // updateUser(id:number,user:User):Observable<User>{
 
-        return <Observable<User>>this.httpClient.put(this.baseUrl+"/updateCustomer/"+id,user)
-    }
-  
-  
+    //     return <Observable<User>>this.httpClient.put(this.baseUrl+"/updateCustomer/"+id,user)
+    // }
+
+    updateBus(id:number,customer:Customer):Observable<Customer>{
+
+      return <Observable<Customer>>this.httpClient.put(this.baseUrl+"/udateCustomer/"+id,customer)
+  }
+//   updateSecurityAlert(id:number,securityAlert:SecurityAlert):Observable<SecurityAlert>{
+ 
+//     return <Observable<SecurityAlert>>this.http.patch(this.baseUrl+"/admin/securityAlert/"+id,securityAlert)
+// }
+updateMobileNumber(id:number,customer:Customer):Observable<Customer>{
+ 
+  return <Observable<Customer>>this.httpClient.patch(this.baseUrl+"/updateCustomerMobileNo/"+id,customer)
+}
+getCustomerById(id:number):Observable<Customer>{
+
+  return <Observable<Customer>>this.httpClient.get(this.baseUrl+"/getCustomer/"+id)
+}
   }
